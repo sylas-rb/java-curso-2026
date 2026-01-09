621bdb16d694e1b00b3444a05519f9c01b5f31ca
sylas-rb <sylasrangel1999@gmail.com>
2026-01-09 20:58:02 -0300

    config: teste inicial de ambiente

---
+++ aula-01-fundamentos/src/TesteConfiguracao.java
@@ -0,0 +1,6 @@
+public class TesteConfiguracao{
+        public static void main(String[] args){
+                System.out.println("Configuração ok!");
+                System.out.println("Sata: "+ new java.util.Date());
+            }
+    }
