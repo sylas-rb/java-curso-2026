630bbcdf13fdbe3afe0b327d61bf3dbfce87df7d
sylas-rb <sylasrangel1999@gmail.com>
2026-01-09 21:55:33 -0300

    aula 01: estrutura limpa, gitignore configurado

--- aula-01-fundamentos/src/TesteConfiguracao.java
+++ aula-01-fundamentos/src/TesteConfiguracao.java
@@ -1,6 +1,10 @@
-public class TesteConfiguracao{
-        public static void main(String[] args){
-                System.out.println("Configuração ok!");
-                System.out.println("Sata: "+ new java.util.Date());
-            }
+import java.util.Date;
+
+public class TesteConfiguracao {
+    public static void main(String[] args) {
+        System.out.println("=== AMBIENTE CONFIGURADO ===");
+        System.out.println("Data: " + new Date());
+        System.out.println("Java: " + System.getProperty("java.version"));
+        System.out.println("=============================");
     }
+}
